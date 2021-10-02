fe29368b95eaacd430b6b95d5494c8663f592b97
TAKEUCHI Yoshitaka <yosh@baud.jp>
2021-10-03 04:18:52 +0900

    add extension

--- phpstan-extension/src/IsLargeIntReturnTypeExtension.php
+++ phpstan-extension/src/IsLargeIntReturnTypeExtension.php
@@ -6,20 +6,44 @@ use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\ErrorType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
 
 class IsLargeIntReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
-	public function isFunctionSupported(MethodReflection $methodReflection): bool
+	public function isFunctionSupported(FunctionReflection $functionReflection): bool
     {
-
+        return $functionReflection->getName() === 'is_large_int';
     }
 
 	public function getTypeFromFunctionCall(
-		FunctionReflection $methodReflection,
-		FuncCall $methodCall,
+		FunctionReflection $functionReflection,
+		FuncCall $functionCall,
 		Scope $scope
 	): Type
     {
+        if (
+            !isset($functionCall->args[0])
+            || !isset($functionCall->args[0]->value)
+        ) {
+            return new ErrorType();
+        }
 
+        $argument = $scope->getType($functionCall->args[0]->value);
+        if ($argument instanceof ConstantIntegerType) {
+            if ($argument->getValue() > 10) {
+                return $argument;
+            } else {
+                return new ConstantBooleanType(false);
+            }
+        }
+        return TypeCombinator::union(
+            new ConstantBooleanType(false),
+            new IntegerType(),
+        );
     }
 }
